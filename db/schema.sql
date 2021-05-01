93ee6ab91dfe635660e8bdfbdf5646af9895d47a
Marina <mgdelneri@gmail.com>
2021-04-30 20:30:23 -0500

    Installed node_modules and required npm packages; Wrote out schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS systemsDB;
+CREATE DATABASE systemsDB;
+
+USE systemsDB;
+
+CREATE TABLE employee (
+    employee_id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (employee_id) 
+);
+
+CREATE TABLE role (
+    role_id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (role_id)
+);
+
+CREATE TABLE department (
+    department_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (department_id)
+)
